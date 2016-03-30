e7a4d43df914b3aa0987742925e00c8c9428010a
heleensnoeck <heleensnoeck@msn.com>
2016-03-30 11:56:41 +0200

    Update sw.js

--- week2/public/sw.js
+++ week2/public/sw.js
@@ -1,28 +1,42 @@
-console.log('im alive');
+//Bron: http://www.html5rocks.com/en/tutorials/service-worker/introduction/
 
-// this.addEventListener('install', function(event) {
-// 	console.log('and  i installed');
-// });
+
+console.log('im alive');
 
 // install
 // welke events wil ik cashen (url's)
-var urlsToCache = [
-	'/',
-	'./styles/style.css',
-	'./js/app.js'
-	 '/images/logo.png',
-     'images/header_1024.jpg'
-];
+// var urlsToCache = [
+//         './',
+//         '/styles/style.css',
+//         '/js/app.js',
+//         '/images/logo.png',
+//         'images/header_1024.jpg'
+// ];
+
+
+// this.addEventListener('install', function(event){
+// 	event.waitUntil(
+// 		cashes.open('look-live-v1') // cashe name aangemaakt en in die case name word met .open alle arrays of files geplaatst
+// 		.then(function(cache) {
+// 	    console.log('opend cache');
+// 	    return cache.addAll(urlsToCache);
+// 		})
+// 	);
+// });
 
 
 this.addEventListener('install', function(event){
-	event.waitUntil(
-		cashes.open('look-live-v1') // cashe name aangemaakt en in die case name word met .open alle arrays of files geplaatst
-		.then(function(cache) {
-	    console.log('opend cache');
-	    return cache.addAll(urlsToCache);
-		})
-	);
+  event.waitUntil(
+    caches.open('looklive-v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        '/styles/style.css',
+        '/js/app.js',
+        '/images/logo.png',
+        'images/header_1024.jpg'
+      ]);
+    })
+  );
 });
 
 this.addEventListener('fetch', function(event) {
@@ -38,17 +52,3 @@ this.addEventListener('fetch', function(event) {
     )
   );
 });
-
-// this.addEventListener('fetch', function(event) {
-// 	var requestURL = new URL(event.request.url);
-
-// 	if(/\.statricflickr\.com$/.test(requestURL.hostname)) {
-// 		event.respondWith(flickrImageResponse(event.request));
-// 	} else {
-// 		event.respondWith(
-// 			caches.match(event.request).then(function(response) {
-// 				return response || fetch (event.request);
-// 			})
-// 		);
-// 	}
-// });
